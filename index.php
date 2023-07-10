2dd781e2b0e35231424f98e0e7f12be950eddb8d
kenser <koffy140221@gmail.com>
2023-07-10 09:20:48 -0600

    crear efectos y eventos DOM

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba de Evento de Teclado</title>
+</head>
+<body>
+    <h1>Prueba de Evento de Teclado</h1>
+    <label for="textBox">Ingrese un texto:</label>
+    <input type="text" id="textBox">
+    <p id="output"></p>
+    <script src="script.js"></script>
+</body>
+</html>
